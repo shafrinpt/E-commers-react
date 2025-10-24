9f1434b9138e17588d5572527a228cbc36283392
Fathima Shafrin <shafrinpt@gmail.com>
2025-10-24 10:49:21 +0530

    setup react routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import {Route, BrowserRouter as Router, Routes,} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Products from './pages/Products'
+import ProductDetails from './pages/ProductDetails'
+import Cart from './pages/Cart'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <div className='text-amber-200'>App</div>
+   <Router>
+     <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+         <Route path='/Products' element={<Products/>} />
+           {/* <Route path='/Product/:id' element={<ProductDetails/>} /> */}
+             <Route path='/cart' element={<Cart/>} />
+      </Routes>
+      <Footer />
+   </Router>
   )
 }
 
--- src/components/QuantityControl.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const QuantityControl = () => {
-  return (
-    <div>QuantityControl</div>
-  )
-}
-
-export default QuantityControl
